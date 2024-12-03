1400a1830ec4c45298ee7898353265ca439a33bf
AdliMs <adlisuryadin@gmail.com>
2024-12-03 14:16:04 +0800

    first commit

---
+++ lyrics.js
@@ -0,0 +1,75 @@
+const nightChanges = [
+    {
+        name: "Goin' out tonight",
+        newLine: true,
+        duration: 2500
+    },
+    {
+        name: "Changes into somethin' red",
+        newLine: true,
+        duration: 2000
+    },
+    {
+        name: "Her mother",
+        newLine: false,
+        duration: 2000
+    },
+    {
+        name: "doesn't like that kind of dress",
+        newLine: true,
+        duration: 2000
+    },
+]
+
+const hopeIsTheThingWithFeathers = [
+    {
+        name: "Heads up",
+        duration: 500
+    },
+    {
+        name: "the wheels are spinning",
+        duration: 500
+    },
+    {
+        name: "Across the plains and valleys deep",
+        duration: 3000
+    },
+    {
+        name: "To dawn",
+        duration: 1000
+    },
+    {
+        name: "the wheels that sing",
+        duration: 1000
+    },
+    {
+        name: "An unending dream",
+        duration: 1000
+    },
+    {
+        name: "Heads up",
+        duration: 1000
+    },
+    {
+        name: "the tracks are running",
+        duration: 1000
+    },
+    {
+        name: "Across the plains where shadows hide",
+        duration: 1000
+    },
+    {
+        name: "We run",
+        duration: 1000
+    },
+    {
+        name: "we stride",
+        duration: 1000
+    },
+    {
+        name: "whoa-whoa",
+        duration: 1000
+    },
+]
+
+export {nightChanges, hopeIsTheThingWithFeathers};
---
+++ main.js
@@ -0,0 +1,28 @@
+import {nightChanges, hopeIsTheThingWithFeathers} from './lyrics.js';
+               
+function shoutIt(text, interval) {
+
+    return new Promise(success => {
+        setTimeout(function() {
+            success(text);
+        }, interval);
+    });
+
+}
+
+async function doSinging(lyrics) {
+    
+    for (let i = 0; i < lyrics.length; i++) {
+        if (lyrics[i].newLine) {
+            const a = await shoutIt(lyrics[i].name, lyrics[i].duration);
+            console.log(a);
+        } else {
+            
+            const a = await shoutIt(lyrics[i].name, lyrics[i].duration);
+            process.stdout.write(`${a} `);
+        }
+    }
+    
+}
+
+doSinging(nightChanges);
