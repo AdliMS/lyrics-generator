947efe505187abf24d5f6d4b11f929330e272673
AdliMs <adlisuryadin@gmail.com>
2024-12-09 05:48:27 +0800

    adding somoe 'changes'

--- lyrics.js
+++ lyrics.js
@@ -2,90 +2,113 @@ const nightChanges = [
     {
         name: "Goin' out tonight",
         newLine: true,
-        duration: 2500
+        duration: 0
     },
     {
         name: "Changes into somethin' red",
         newLine: true,
-        duration: 2000
+        duration: 1800
     },
     {
-        name: "Her mother",
+        name: "Her mother,",
         newLine: false,
         duration: 2000
     },
     {
         name: "doesn't like that kind of dress",
         newLine: true,
-        duration: 2000
+        duration: 1800
+    },
+    {
+        name: "Everything she never had",
+        newLine: true,
+        duration: 2200
+    },
+    {
+        name: "she's showin' off",
+        newLine: true,
+        duration: 3000
     },
 ]
 
 const hopeIsTheThingWithFeathers = [
     {
-        name: "\nHeads up!",
+        name: "Heads up!",
         newLine: false,
-        duration: 0
+        duration: 0,
+        clearScreen: false
     },
     {
         name: "the wheels are spinning",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "Across the plains and valleys deep",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: true
     },
     {
         name: "To dawn",
         newLine: true,
-        duration: 3800
+        duration: 3800,
+        clearScreen: false
     },
     {
         name: "the wheels that sing",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "An unending dream",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "Heads up!",
         newLine: false,
-        duration: 3800 // kk
+        duration: 3800,
+        clearScreen: true
     },
     {
         name: "the tracks are running",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "Across the plains where shadows hide",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "We run",
         newLine: true,
-        duration: 3800
+        duration: 3800,
+        clearScreen: true
     },
     {
         name: "we stride",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "whoa-whoa",
         newLine: true,
-        duration: 1800
+        duration: 1800,
+        clearScreen: false
     },
     {
         name: "",
         newLine: false,
-        duration: 3800
+        duration: 3800,
+        clearScreen: false
     },
 ]
 
--- main.js
+++ main.js
@@ -1,5 +1,47 @@
 import {nightChanges, hopeIsTheThingWithFeathers} from './lyrics.js';
-               
+import * as fs from 'node:fs';
+import * as readline from 'node:readline';
+import { EventEmitter } from 'node:events';
+
+// Import module
+import { lyrics, lyricsv2 } from '@bochilteam/scraper-lyrics'
+
+const data = await lyricsv2('Bohemian Rhapsody')
+console.log(data) // JSON
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+function createLyrics() {
+    rl.question('Masukkan judul lagu: ', (songTitle) => {
+        rl.question('Masukkan Penggalan lirik: ', (sentence) => {
+            rl.question('Tentukan detik(s) waktu kemunculan liriknya : ', (interval) => {
+                rl.question('Sambung lirik ini dengan penggalan lirik setelahnya? (yes/no): ', (newLine) => {
+                    
+                    if (newLine === 'yes'){
+                        newLine = true;
+                    } else {
+                        newLine = false;
+                    }
+    
+                    const lyricsData = { sentence, interval, newLine };
+        
+                    const file = JSON.parse(fs.readFileSync(`lyrics/someLyrics.json`, 'utf8'));
+                    file.push(lyricsData);
+                    fs.writeFileSync('lyrics/someLyrics.json', JSON.stringify(file));
+    
+                    rl.close();
+    
+                })
+            })
+        })
+    })
+}
+
+// createLyrics();
+         
 function shoutIt(text, interval) {
 
     return new Promise(success => {
@@ -7,22 +49,33 @@ function shoutIt(text, interval) {
             success(text);
         }, interval);
     });
+}
 
+async function loadingScreen(...args) {
+
+    // const dots = ['. ', ' . ', ' . ', ' .\n'];
+    for (let i = 0; i < args.length; i++) {
+        const a = await shoutIt(args[i], 1000);
+        process.stdout.write(`${a} `);
+    }
+    process.stdout.write('\u001B[2J\u001B[0;0f');
 }
 
 async function doSinging(lyrics) {
-    
+
+    await loadingScreen('. ', ' . ', ' . ', ' .');
+    process.stdout.write('\u001B[2J\u001B[0;0f');
     for (let i = 0; i < lyrics.length; i++) {
+
         if (lyrics[i].newLine) {
             const a = await shoutIt(lyrics[i].name, lyrics[i].duration);
-            console.log(a);
+            console.log(a);   
         } else {
-            
             const a = await shoutIt(lyrics[i].name, lyrics[i].duration);
             process.stdout.write(`${a} `);
+            }
         }
-    }
-    
+    await loadingScreen('. ', ' . ', ' . ', ' . ', ' . ', ' . ');
+    process.exit();
 }
-
-doSinging(hopeIsTheThingWithFeathers);
+// doSinging(nightChanges);
